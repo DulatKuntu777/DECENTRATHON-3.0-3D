61a4f721ef758954b3f3643331def4e152a56c0c
DulatKuntu777 <dulatkuntu7@gmail.com>
2024-10-19 00:49:54 +0500

    Add vehicle

---
+++ Assets/_InternalAssets/Code/AI/AI.cs
@@ -0,0 +1,52 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+    
+public class AI : SerializedMonoBehaviour
+    {
+        public Visor visor;
+
+        public float inputbrake = 0;
+        public float inputSteer = 0;
+        public float inputThrottle = 0;
+        public float inputBoost = 0;
+
+        [SerializeField] private SCC_Drivetrain carControl;
+        [SerializeField] private SCC_InputProcessor inputs;
+        public bool brake = true;
+        
+        private void Awake()
+        {
+            OnValidate();
+        }
+
+        private void OnValidate()
+        {
+            carControl ??= GetComponent<SCC_Drivetrain>();
+            inputs ??= GetComponent<SCC_InputProcessor>();
+            visor ??= GetComponentInChildren<Visor>();
+        }
+        private void OnEnable()
+        {
+            visor.condition = true;
+        }
+        private void Start()
+        {
+            inputs.receiveInputsFromInputManager = false;
+        }
+
+        private bool useNos = false;    
+        void Update()
+        {
+            if (brake)
+            {
+                inputs.inputs.brakeInput = 1;
+                return;
+            }
+
+            inputSteer = visor.steerValue;
+            inputThrottle = 1;
+
+            inputs.inputs.steerInput = inputSteer;
+            inputs.inputs.throttleInput = inputThrottle;
+        }
+    }
